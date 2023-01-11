c9f8a82476f98ca00de5074d119fdbaa63147ff7
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-11 09:24:15 -0500

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+  verificador = false;
+  subtitulo!:string;
+
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      nombre: ['',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern('[a-zA-Z ]*')
+        ]
+      ],
+      dni: ['',
+        [
+          Validators.required,
+        ]
+      ],
+      estado_civil: ['',
+        [
+          Validators.required,
+        ]
+      ]
+    })
+  }
+
+  send(): any {
+    this.subtitulo = (this.formLogin.value);
+    this.verificador = true;
+  }
+
+}
